be7d2b924b7794df6fd4afc5fe59a854f78b0ae7
NihalRawat <127190177+NihalRawat@users.noreply.github.com>
2023-08-22 15:50:55 +0530

    done

---
+++ src/com/onlineshop/db/database.java
@@ -0,0 +1,9 @@
+package com.onlineshop.db;
+
+public interface database 
+{
+	String driver="com.mysql.cj.jdbc.Driver";
+	String url="jdbc:mysql://localhost:3307/onlineshopping";
+	String username="root";
+	String password="root";
+}
